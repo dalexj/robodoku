ac5b11d5a2eb5e6a24609d4d7c4b335801d5fd1d
Alex <96dalex@gmail.com>
2015-03-20 12:37:34 -0600

    added square finder

--- lib/solver.rb
+++ lib/solver.rb
@@ -1,2 +1,17 @@
 class Solver
+  def initialize(puzzle)
+    @puzzle = puzzle
+  end
+
+  def solve
+    parsed_puzzle
+  end
+
+  def parsed_puzzle
+    @parsed_puzzle ||= @puzzle.split("\n").map(&:chars)
+  end
+
+  def square_from(x, y)
+    parsed_puzzle[(x/3)*3, 3].flat_map { |line| line[(y/3)*3, 3] }
+  end
 end
--- test/solver_test.rb
+++ test/solver_test.rb
@@ -2,7 +2,29 @@ require_relative "test_helper"
 require_relative "../lib/solver"
 
 class SolverTest < Minitest::Test
-  def test_it_works
-    assert Solver.new
+  def test_it_solves_puzzle
+    skip
+    puzzle   = File.read("test/puzzle1.txt").strip
+    solution = File.read("test/puzzle1_solution.txt").strip
+    assert_equal solution, Solver.new(puzzle).solve
+  end
+
+  def test_it_gets_square
+    puzzle   = <<-PUZZLE
+111222333
+111222333
+111222333
+444555666
+444555666
+444555666
+777888999
+777888999
+777888999
+    PUZZLE
+    assert_equal ["1"] * 9, Solver.new(puzzle).square_from(2, 2)
+    assert_equal ["9"] * 9, Solver.new(puzzle).square_from(6, 8)
+    assert_equal ["7"] * 9, Solver.new(puzzle).square_from(8, 1)
+    assert_equal ["5"] * 9, Solver.new(puzzle).square_from(5, 5)
+    assert_equal ["6"] * 9, Solver.new(puzzle).square_from(5, 8)
   end
 end
